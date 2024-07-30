83274442192e2c46bef602d13d66547e14ecd9bb
Sumant Kumar <sumantkumar@airasia.com>
2024-07-30 13:53:38 +0530

    updated test case as per latest upadte in apply application form

--- Pages/MOHSApplicationPage.cs
+++ Pages/MOHSApplicationPage.cs
@@ -10,6 +10,10 @@ namespace SeleniumNUnit.Pages
         {
             this.driver = driver;
         }
+        //Note Before Application
+
+        private IWebElement NoteBeforeApplicationNextButton => driver.FindElement(By.XPath("(//button[contains(@aria-label, 'Next')])[1]"));
+
         //Parent information
         private IWebElement ParentFirstNameField => driver.FindElement(By.XPath("//input[@complink='Name_First']"));
         private IWebElement ParentLastNameField => driver.FindElement(By.XPath("//input[@complink='Name_Last']"));
@@ -18,7 +22,7 @@ namespace SeleniumNUnit.Pages
         private IWebElement SecondParentDropDown => driver.FindElement(By.Id("select2-Dropdown-arialabel-container"));
         private IWebElement HearFromSearchEngineCheckBox => driver.FindElement(By.CssSelector("[for='Checkbox_1']"));
         private IWebElement PreferredStartDateCalanderField => driver.FindElement(By.CssSelector("input#Date-date"));
-        private IWebElement ParentNextButton => driver.FindElement(By.XPath("(//button[contains(@aria-label, 'Next')])[1]"));
+        private IWebElement ParentNextButton => driver.FindElement(By.XPath("(//button[contains(@aria-label, 'Next')])[2]"));
 
         //Student Information
         private IWebElement NumberOfStudentDropDown => driver.FindElement(By.Id("select2-Dropdown1-arialabel-container"));
@@ -27,17 +31,19 @@ namespace SeleniumNUnit.Pages
         private IWebElement StudentLastNameField => driver.FindElement(By.XPath("//input[@complink='Name2_Last']"));
         private IWebElement StudentEmailField => driver.FindElement(By.Id("Email2-arialabel"));
         private IWebElement StudentPhoneField => driver.FindElement(By.Name("PhoneNumber2"));
-        private IWebElement StudentDOBField => driver.FindElement(By.Id("SingleLine3-arialabel"));
+        private IWebElement StudentDOBField => driver.FindElement(By.Id("Date3-date"));
         private IWebElement StudentGenderDropDown => driver.FindElement(By.Id("select2-Dropdown3-arialabel-container"));
         private IWebElement StudentGenderMale => driver.FindElement(By.XPath("//li[text()='Male']"));
         private IWebElement MiaPreAccountConfirmationDropDown => driver.FindElement(By.Id("select2-Dropdown4-arialabel-container"));
+         private IWebElement MostRecentGradeField => driver.FindElement(By.CssSelector("input#Number1-arialabel"));
         private IWebElement StudentCurrentSchoolingDropDown => driver.FindElement(By.Id("select2-Dropdown5-arialabel-container"));
+        private IWebElement MotivationToApplyMOHSChildNeedFlexibleSchedule => driver.FindElement(By.CssSelector("[for='Checkbox7_1']"));
         private IWebElement Homeschool => driver.FindElement(By.XPath("//li[text()='Homeschool']"));
         private IWebElement Algebra1Checkbox => driver.FindElement(By.XPath("//*[text()='Algebra 1']"));
         private IWebElement English1Checkbox => driver.FindElement(By.XPath("//*[text()='English 1']"));
         private IWebElement PhysicsCheckbox => driver.FindElement(By.XPath("//*[text()='Physics']"));
         private IWebElement StudentChallangeDropDown => driver.FindElement(By.Id("select2-Dropdown13-arialabel-container"));
-        private IWebElement StudentNextButton => driver.FindElement(By.XPath("(//button[contains(@aria-label, 'Next')])[2]"));
+        private IWebElement StudentNextButton => driver.FindElement(By.XPath("(//button[contains(@aria-label, 'Next')])[3]"));
 
         //Finacial Aid Application Element Locators
         private IWebElement FinancialAidDropDown => driver.FindElement(By.Id("select2-Dropdown12-arialabel-container"));
@@ -52,6 +58,7 @@ namespace SeleniumNUnit.Pages
 
         public void FillMOHSForm()
         {
+            this.NoteBeforeApplicationNextButton.Click();
             this.FillParentInformation();
             this.FillStudentInformation();
             this.FillFinancialAidApplication();
@@ -67,8 +74,9 @@ namespace SeleniumNUnit.Pages
             this.DropDownYes.Click();
             this.HearFromSearchEngineCheckBox.Click();
             this.PreferredStartDateCalanderField.Click();
-            this.PreferredStartDateCalanderField.SendKeys("30-Jul-2024");
+            this.PreferredStartDateCalanderField.SendKeys("30-Aug-2024");
             this.ParentNextButton.Click();
+            
         }
 
         public void FillStudentInformation()
@@ -79,16 +87,19 @@ namespace SeleniumNUnit.Pages
             this.StudentLastNameField.SendKeys("SLastName");
             this.StudentEmailField.SendKeys("Student@gmail.com");
             this.StudentPhoneField.SendKeys("7406507634");
-            this.StudentDOBField.SendKeys("12/04/1999");
+            this.StudentDOBField.SendKeys("12-Apr-1999");
+            Thread.Sleep(1000);
             this.StudentGenderDropDown.Click();
             Thread.Sleep(1000);
             this.StudentGenderMale.Click();
             this.MiaPreAccountConfirmationDropDown.Click();
             Thread.Sleep(1000);
             this.DropDownNo.Click();
-            this.StudentCurrentSchoolingDropDown.Click();
-            Thread.Sleep(1000);
-            this.Homeschool.Click();
+            this.MostRecentGradeField.SendKeys("12");
+            // this.StudentCurrentSchoolingDropDown.Click(); // this field not availabe any more
+            // Thread.Sleep(1000);
+            // this.Homeschool.Click();   
+            this.MotivationToApplyMOHSChildNeedFlexibleSchedule.Click();
             this.Algebra1Checkbox.Click();
             this.English1Checkbox.Click();
             this.PhysicsCheckbox.Click();
@@ -96,6 +107,8 @@ namespace SeleniumNUnit.Pages
             this.StudentChallangeDropDown.Click();
             Thread.Sleep(1000);
             this.DropDownNo.Click();
+
+             Thread.Sleep(2000);
             this.StudentNextButton.Click();
         }
 
